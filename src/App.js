3bc24f36ef463215302ecc3ed9fd7f83d0c6b31b
Denojan <it21224034@my.sliit.lk>
2023-07-16 12:07:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
         {/* <Route path="/" element={<Layout />}> */}
           {/* public routes */}
           <Route path="/" element={<Login />} />
-          <Route path="register" element={<Register />} />
+          <Route path="/login/register" element={<Register />} />
           <Route path="unauthorized" element={<Unauthorized />} />
 
           {/* we want to protect these routes */}
